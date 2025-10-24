209c66ffa42d29b259bf54b49cb74b1a0e470d5e
Efrosiniya24 <efrosiniya24@gmail.com>
2025-10-24 16:47:07 +0300

    auth

---
+++ src/pages/auth/listener/signUp.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { useNavigate, NavLink, useLocation } from "react-router-dom";
+import login from "./login.module.css";
+import logo from "../../../logo.png";
+
+const SignUp = () => {
+  return (
+    <div className={login.mainLogin}>
+      <div className={login.form}>
+        <div className={login.formElements}>
+          <div className={login.logoSection}>
+            <img src={logo} />
+            <h1 className={login}>Space</h1>
+          </div>
+          <div className={login.inputSectionButton}>
+            <div className={login.input}>
+              <div className={login.inputSectoion}>
+                <p>Пароль</p>
+                <input
+                  type="password"
+                  йы
+                  name="password"
+                  placeholder="Введите пароль"
+                  // value={email}
+                  // onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className={login.inputSectoion}>
+                <p>Пароль</p>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Введите пароль"
+                  // value={email}
+                  // onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+            <button>Зарегистрироваться</button>
+          </div>
+        </div>
+        <div className={login.choice}>
+          <p>Нет аккаунта?</p>
+          <a>Зарегистрируйся</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
